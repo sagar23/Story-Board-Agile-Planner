f81cf66a4a7f7d163e242c6d116d43a3a3db3401
Sagar Dewan <sagar.dewan.sd@gmail.com>
2023-08-17 18:31:08 -0400

    Update card.component.ts

--- src/app/card.component/card.component.ts
+++ src/app/card.component/card.component.ts
@@ -4,7 +4,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { cardDialogComponent } from '../card.dialog.component/card.dialog.component'
 
 @Component({
-  selector: 'card',
+  selector: `card`,
   templateUrl: 'card.component.html',
   styleUrls: ['card.component.css'],
 
